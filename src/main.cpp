5073d808f82455958cc7854d3c6aed2f1770f634
Mopigames <mael1.prego@epitech.eu>
2026-04-07 14:16:34 +0200

    remove broad SliderTouchLogic hook, add iOS support

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,8 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/PauseLayer.hpp>
 #include <Geode/modify/OptionsLayer.hpp>
-#include <Geode/modify/SliderTouchLogic.hpp>
 
 using namespace geode::prelude;
-
-enum class SliderType { None, Music, SFX };
-static SliderType s_lastSliderType = SliderType::None;
-static float s_lastLinear = 0.f;
-static float s_lastCurved = 0.f;
 
 static float applyExponentialCurve(float linear) {
     if (linear <= 0.0f) return 0.0f;
@@ -31,43 +25,29 @@ static void handleMusicSlider(SliderThumb* thumb) {
     float linear = thumb->getValue();
     float curved = applyExponentialCurve(linear);
 
+    if (Mod::get()->getSettingValue<bool>("logging")) {
+        log::info("Music: slider={:.1f}% -> actual={:.1f}%", linear * 100.f, curved * 100.f);
+    }
+
     auto* engine = FMODAudioEngine::sharedEngine();
     GameManager::get()->m_bgVolume = linear;
     engine->setBackgroundMusicVolume(curved);
     engine->m_musicVolume = linear;
-
-    s_lastSliderType = SliderType::Music;
-    s_lastLinear = linear;
-    s_lastCurved = curved;
 }
 
 static void handleSfxSlider(SliderThumb* thumb) {
     float linear = thumb->getValue();
     float curved = applyExponentialCurve(linear);
 
+    if (Mod::get()->getSettingValue<bool>("logging")) {
+        log::info("SFX: slider={:.1f}% -> actual={:.1f}%", linear * 100.f, curved * 100.f);
+    }
+
     auto* engine = FMODAudioEngine::sharedEngine();
     GameManager::get()->m_sfxVolume = linear;
     engine->setEffectsVolume(curved);
     engine->m_sfxVolume = linear;
-
-    s_lastSliderType = SliderType::SFX;
-    s_lastLinear = linear;
-    s_lastCurved = curved;
 }
-
-class $modify(SliderTouchLogic) {
-    void ccTouchEnded(cocos2d::CCTouch* touch, cocos2d::CCEvent* event) {
-        SliderTouchLogic::ccTouchEnded(touch, event);
-
-        if (s_lastSliderType != SliderType::None &&
-            Mod::get()->getSettingValue<bool>("logging")) {
-            const char* name = s_lastSliderType == SliderType::Music ? "Music" : "SFX";
-            log::info("{}: slider={:.1f}% -> actual={:.1f}%",
-                name, s_lastLinear * 100.f, s_lastCurved * 100.f);
-        }
-        s_lastSliderType = SliderType::None;
-    }
-};
 
 class $modify(MyPauseLayer, PauseLayer) {
     void customSetup() {
